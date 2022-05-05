1cfd36781620857dc800faee872ae857c8d729f4
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 15:05:28 +0100

    Return JSON, status codes, and added url variables

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello from the Planetary API!'
+    return jsonify(message='Hello from the Planetary API!'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(
+            message="Sorry " + name + ", you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name)
 
 
 if __name__ == '__main__':
