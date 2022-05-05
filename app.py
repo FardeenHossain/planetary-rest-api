4ac8a120bc4cb1099b2b157245ee3900358c749f
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 16:51:11 +0100

    Removed unnecessary functions

--- app.py
+++ app.py
@@ -57,31 +57,6 @@ def home():
     return jsonify(message='Hello from the Planetary API!'), 200
 
 
-@app.route('/not_found')
-def not_found():
-    return jsonify(message='That resource was not found'), 404
-
-
-@app.route('/parameters')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(
-            message="Sorry " + name + ", you are not old enough"), 401
-    else:
-        return jsonify(message="Welcome " + name)
-
-
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-    if age < 18:
-        return jsonify(
-            message="Sorry " + name + ", you are not old enough"), 401
-    else:
-        return jsonify(message="Welcome " + name)
-
-
 @app.route('/planets', methods=['GET'])
 def planets():
     planets_list = Planet.query.all()
