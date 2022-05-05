d8f49c03444d208c846f3e6df9d9c1f764b72c1e
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 17:03:23 +0100

    Added additional comments

--- app.py
+++ app.py
@@ -19,18 +19,23 @@ jwt = JWTManager(app)
 
 @app.cli.command('db_create')
 def db_create():
+    """Create database CLI"""
     db.create_all()
     print('Database created!')
 
 
 @app.cli.command('db_drop')
 def db_drop():
+    """Drop database CLI"""
     db.drop_all()
     print('Database dropped!')
 
 
 @app.cli.command('db_seed')
 def db_seed():
+    """Seed database CLI"""
+
+    # Define class data
     earth = Planet(planet_name='Earth', mass=5.972e24, radius=6371,
                    distance=149.6e6)
 
@@ -54,11 +59,13 @@ def db_seed():
 
 @app.route('/')
 def home():
+    """Home endpoint"""
     return jsonify(message='Hello from the Planetary API!'), 200
 
 
 @app.route('/planets', methods=['GET'])
 def planets():
+    """Planets endpoint"""
     planets_list = Planet.query.all()
     result = planets_schema.dump(planets_list)
     return jsonify(result)
@@ -66,10 +73,15 @@ def planets():
 
 @app.route('/planets/<int:planet_id>', methods=['GET'])
 def planet_details(planet_id: int):
+    """Planet details endpoint"""
+
+    # Check if planet exists
     planet = Planet.query.filter_by(planet_id=planet_id).first()
+
     if planet:
         result = planet_schema.dump(planet)
         return jsonify(result)
+
     else:
         return jsonify(message="That planet does not exists"), 404
 
@@ -77,11 +89,17 @@ def planet_details(planet_id: int):
 @app.route('/add_planet', methods=['POST'])
 @jwt_required()
 def add_planet():
+    """Add planet endpoint"""
+
+    # Check if planet exists
     planet_name = request.form['planet_name']
     test = Planet.query.filter_by(planet_name=planet_name).first()
+
     if test:
         return jsonify(message="That planet already exists"), 409
+
     else:
+        # Request form data
         mass = float(request.form['mass'])
         radius = float(request.form['radius'])
         distance = float(request.form['distance'])
@@ -97,14 +115,20 @@ def add_planet():
 @app.route('/update_planet/<int:planet_id>', methods=['PUT'])
 @jwt_required()
 def update_planet(planet_id: int):
+    """Update planet endpoint"""
+
+    # Check if planet exists
     planet = Planet.query.filter_by(planet_id=planet_id).first()
+
     if planet:
+        # Request form data
         planet.planet_name = request.form['planet_name']
         planet.mass = float(request.form['mass'])
         planet.radius = float(request.form['radius'])
         planet.distance = float(request.form['distance'])
         db.session.commit()
         return jsonify(message="Planet updated"), 202
+
     else:
         jsonify(message="That planet does not exist"), 404
 
@@ -112,22 +136,33 @@ def update_planet(planet_id: int):
 @app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
 @jwt_required()
 def remove_planet(planet_id: int):
+    """Remove planet endpoint"""
+
+    # Check if planet exists
     planet = Planet.query.filter_by(planet_id=planet_id).first()
+
     if planet:
         db.session.delete(planet)
         db.session.commit()
         return jsonify(message="Planet removed"), 202
+
     else:
         return jsonify(message="That planet does not exist"), 404
 
 
 @app.route('/register', methods=['POST'])
 def register():
+    """Register endpoint"""
+
+    # Check if email exists
     email = request.form['email']
     test = User.query.filter_by(email=email).first()
+
     if test:
         return jsonify(message='That email already exists'), 409
+
     else:
+        # Request form data
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         password = request.form['password']
@@ -140,23 +175,30 @@ def register():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """Login endpoint"""
+
+    # Request form data
     if request.is_json:
         email = request.json['email']
         password = request.json['password']
+
     else:
         email = request.form['email']
         password = request.form['password']
 
+    # Check if email and password exist
     test = User.query.filter_by(email=email, password=password).first()
+
     if test:
         access_token = create_access_token(identity=email)
         return jsonify(message="Login succeeded!", access_token=access_token)
+
     else:
         return jsonify(message="Invalid email or password"), 401
 
 
-# Database models
 class User(db.Model):
+    """User database class"""
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True)
     first_name = Column(String)
@@ -166,6 +208,7 @@ class User(db.Model):
 
 
 class Planet(db.Model):
+    """Planet database class"""
     __tablename__ = 'planets'
     planet_id = Column(Integer, primary_key=True)
     planet_name = Column(String)
@@ -176,11 +219,13 @@ class Planet(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
+        """User marshmallow class"""
         fields = ('user_id', 'first_name', 'last_name', 'email', 'password')
 
 
 class PlanetSchema(ma.Schema):
     class Meta:
+        """Planet marshmallow class"""
         fields = ('planet_id', 'name', 'mass', 'radius', 'distance')
 
 
