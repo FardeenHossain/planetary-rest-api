c66c3e2406e9f5d23b02d8a88ff7d4852643efc7
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 16:19:23 +0100

    Added login endpoint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from sqlalchemy import Column, Integer, String, Float
 import os
 
@@ -9,9 +10,11 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
     basedir, 'planets.db')
+app.config['JWT_SECRET_KEY'] = 'secret-key'  # Change this IRL
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 
 @app.cli.command('db_create')
@@ -101,6 +104,23 @@ def register():
         db.session.add(user)
         db.session.commit()
         return jsonify(message="User created successfully"), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login succeeded!", access_token=access_token)
+    else:
+        return jsonify(message="Invalid email or password"), 401
 
 
 # Database models
