3ca75148a1341744a9462ee6d4836d1ea414707e
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 15:19:15 +0100

    Added database classes

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+    basedir, 'planets.db')
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -31,6 +40,25 @@ def url_variables(name: str, age: int):
             message="Sorry " + name + ", you are not old enough"), 401
     else:
         return jsonify(message="Welcome " + name)
+
+
+# Database models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == '__main__':
