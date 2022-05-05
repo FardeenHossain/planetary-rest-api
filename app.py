7b37d016f5bfdb528844c3ef51b1f8f9ea5a3282
Fardeen Hossain <fardeen.k.hossain@gmail.com>
2022-05-05 15:09:52 +0100

    Added conversion filters

--- app.py
+++ app.py
@@ -24,5 +24,14 @@ def parameters():
         return jsonify(message="Welcome " + name)
 
 
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(
+            message="Sorry " + name + ", you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name)
+
+
 if __name__ == '__main__':
     app.run()
